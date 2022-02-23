8053c94f00749171205a8b69012e0f76c2c0dad1
Llanes520 <llanesdeivid93@gmail.com>
2022-02-23 08:28:23 -0500

    Proyecto Prueba

---
+++ PruebaGit/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PruebaGit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Algoritmo4();
+        }
+
+        static void Algoritmo4()
+        {
+            int num1, num2;
+
+            List<int> list = new List<int>();
+
+            Console.WriteLine("Ingrese dos numeros enteros");
+            Console.WriteLine("El primero menor y el siguiente mayor: ");
+
+            num1 = Convert.ToInt32(Console.ReadLine());
+            num2 = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = num1; i < num2; i++)
+            {
+                if (i == num1)
+                {
+                    i++;
+                }
+                num1++;
+                list.Add(num1);
+                Console.WriteLine(num1);
+            }
+        }
+}
